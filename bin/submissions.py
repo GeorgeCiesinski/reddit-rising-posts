d947f7e4a13f266a58c4ed12ee56bbf6f5badf24
George Ciesinski <george.ciesin@gmail.com>
2019-06-29 19:25:10 -0400

    Organized into a class structure.

--- bin/submissions.py
+++ bin/submissions.py
@@ -11,16 +11,21 @@ client_secret = "DX87ZhsDhvJrvxdoud0CXmcbLGA"
 import praw
 
 
-# Reddit API Login
-r = praw.Reddit(client_id="Zfl37rh1asVTjQ",
-                client_secret="DX87ZhsDhvJrvxdoud0CXmcbLGA",
-                username="top10tracket",
-                password="k2T%5VuSJc8k",
-                user_agent="reddit-rising-posts")
+class Reddit:
+
+    def api_login(self):
+
+        # Reddit API Login
+        r = praw.Reddit(client_id="Zfl37rh1asVTjQ",
+                        client_secret="DX87ZhsDhvJrvxdoud0CXmcbLGA",
+                        username="top10tracket",
+                        password="k2T%5VuSJc8k",
+                        user_agent="reddit-rising-posts")
+
 
 # Subreddits
 subreddit = input("Which Subreddit do you want to look in? ")
-sr = r.subreddit(subreddit)
+sr = Reddit.r.subreddit(subreddit)
 
 
 if __name__ == "__main__":
