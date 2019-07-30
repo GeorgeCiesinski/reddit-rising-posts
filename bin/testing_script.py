0b285793596e6a5d2849ca178da5e1c9ef793284
George Ciesinski <george.ciesin@gmail.com>
2019-07-30 13:25:54 -0400

    Submission.py is working but requires further testing

--- bin/testing_script.py
+++ bin/testing_script.py
@@ -1,4 +1,5 @@
 import praw
+from Submission import Submission
 import sys
 
 """
@@ -44,18 +45,35 @@ if __name__ == "__main__":
 
 	subreddit = r.reddit.subreddit('jokes')
 	print("The display name is: " + subreddit.display_name)
-	# print("The subreddit title is: " + subreddit.title)
 
-	submission = r.reddit.submission(url="https://www.reddit.com/r/Jokes/comments/cjersb/i_went_into_a_pet_shop_and_asked_for_twelve_bees/")
-	print("The submission title is: " + submission.title + "\n")
-	print("The submission id is: " + submission.id + "\n")
-	print("The submission body contains: \n" + submission.selftext + "\n")
-	print("The number of comments is: " + str(submission.num_comments))
-	print("The score is: " + str(submission.score))
-	print("The author is: " + str(submission.author))
-	print("This post was created on: " + str(submission.created_utc))
-	print("The url is: " + submission.url)
-	print("The subreddit is: " + str(submission.subreddit))
+	submission_PRAW = r.reddit.submission(url="https://www.reddit.com/r/Jokes/comments/cjersb/i_went_into_a_pet_shop_and_asked_for_twelve_bees/")
+
+	# PRAW Submission Object Test
+	print("\nUSING PRAW SUBMISSION OBJECT \n")
+	print("The submission title is: " + submission_PRAW.title)
+	print("The submission id is: " + submission_PRAW.id)
+	print("The submission body contains: \n\n" + submission_PRAW.selftext + "\n")
+	print("The number of comments is: " + str(submission_PRAW.num_comments))
+	print("The score is: " + str(submission_PRAW.score))
+	print("The author is: " + str(submission_PRAW.author))
+	print("This post was created on: " + str(submission_PRAW.created_utc))
+	# TODO: Research correct method to convert UTC time into readible format
+	print("The url is: " + submission_PRAW.url)
+	print("The subreddit is: " + str(submission_PRAW.subreddit))
+
+	# Our Submission Object
+	s = Submission(submission_PRAW)
+	print("\nUSING OUR OWN SUBMISSION OBJECT \n")
+	print("The submission title is: " + s.title)
+	print("The submission id is: " + s.id)
+	print("The submission body contains: \n\n" + s.selftext + "\n")
+	print("The number of comments is: " + str(s.num_comments))
+	print("The score is: " + str(s.score))
+	print("The author is: " + str(s.author))
+	print("This post was created on: " + str(s.created_utc))
+	# TODO: Research correct method to convert UTC time into readible format
+	print("The url is: " + s.url)
+	print("The subreddit is: " + str(s.subreddit))
 
 
 
