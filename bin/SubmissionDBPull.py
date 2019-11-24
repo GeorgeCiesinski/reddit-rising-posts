a03425d109cec07a5d36eca894f48529d4693c61
George Ciesinski <george.ciesin@gmail.com>
2019-11-24 17:54:38 -0500

    Started coding Submission DB Pull

--- bin/SubmissionDBPull.py
+++ bin/SubmissionDBPull.py
@@ -1,3 +1,20 @@
+import signal
+from sys import exit
+
+from bin.LIB import LIB
+from bin.DAL import *
+
+
 class SubmissionDBPull:
+
+    def sig_handler(self, sig, frame):
+        """
+        Signal handler will stop the main while loop
+        :param sig:  The signal that was received
+        :param frame: The execution stack frame
+        :return:
+        """
+        self.keep_ruinning = False
+
     def __init__(self):
         pass
