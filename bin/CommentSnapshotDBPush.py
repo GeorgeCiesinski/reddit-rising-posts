566a7dd4ce260d68675778479465a02d286718a7
George Ciesinski <george.ciesin@gmail.com>
2020-01-29 19:16:43 -0500

    Started new functions

--- bin/CommentSnapshotDBPush.py
+++ bin/CommentSnapshotDBPush.py
@@ -1,3 +1,6 @@
-class CommentSnapshotDBPush:
-    def __init__(self):
-        pass
+"""
+---> Input: Submission Object
+---> Function:
+For each comment: Call Robbie's Function: Dal.Comments.comment_snapshot_insert
+---> Output: True or False
+"""
